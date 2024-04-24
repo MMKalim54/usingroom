6b3231fa0ebaac678646980d9f98cf3cdc75f88a
MMKalim54 <mmkalim54@gmail.com>
2024-04-24 10:23:56 +0500

    first commit

--- server.js
+++ server.js
@@ -20,10 +20,15 @@ io.on('connection', socket => {
     socket.join(roomId)
     socket.to(roomId).broadcast.emit('user-connected', userId)
 
+    
     socket.on('disconnect', () => {
       socket.to(roomId).broadcast.emit('user-disconnected', userId)
     })
   })
 })
 
-server.listen(3000)
+var server_port = process.env.YOUR_PORT || process.env.PORT || 5000;
+var server_ip = '0.0.0.0'; // Listen on all available network interfaces
+server.listen(server_port, server_ip, () => {
+    console.log("Server started on port " + server_port);
+});
